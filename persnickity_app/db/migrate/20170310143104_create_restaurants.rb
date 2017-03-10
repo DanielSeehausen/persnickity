20568893c8e142c62d4fe52d812bc899020797da
Devin Rieger <dev0089@gmail.com>
2017-03-10 10:30:14 -0500

    ran migrate and debugged migrations

--- persnickity_app/db/migrate/20170310143104_create_restaurants.rb
+++ persnickity_app/db/migrate/20170310143104_create_restaurants.rb
@@ -2,7 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration[5.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.varchar{10} :grade
+      t.string :grade, limit: 10
       t.integer :zip, limit: 5
 
       t.timestamps
