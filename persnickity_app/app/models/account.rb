c4d4dbbd77d139e7c1111c6038845ea971f69322
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-03-13 17:42:57 -0400

    Update account.rb
    removed validates zip code presence true

--- persnickity_app/app/models/account.rb
+++ persnickity_app/app/models/account.rb
@@ -2,7 +2,6 @@ class Account < ApplicationRecord
   has_secure_password
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :user_name, presence: true, uniqueness: true
-  validates :zip_code, presence: true
   has_one :neighborhood
   after_create :assign_neighborhood_id_from_zip
 
