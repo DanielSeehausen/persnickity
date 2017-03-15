48d69ff415ca63a7e698508c4e5c3fcadd64c433
Daniel Seehausen <daniel.seehausen@gmail.com>
2017-03-15 09:08:17 -0400

    Minor changes to 404 timing

--- app/assets/javascripts/not_found.js
+++
@@ -1,7 +0,0 @@
-function bindEvent(e, eventName, callback) {
-    e.addEventListener(eventName, callback, false);
-};
-
-bindEvent(document.body, 'scroll', function(e) {
-    document.body.scrollLeft = 0;
-});
